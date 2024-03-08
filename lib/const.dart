bfffa39eb258bb1925760fa974f6c975f370399a
IrfanFared <akunytberita527>
2024-03-08 15:29:40 +0700

    hari ke 3 belajar dart dasar

---
+++ lib/const.dart
@@ -0,0 +1,11 @@
+void main() {
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3, 4];
+
+  array1[0] = 7;
+  /*array2[0] = 8; jadi kalo kata kunci cost itu isinya isi dan nama variable tidak bis
+  bisa si deklasikan ulang dan tidak bisa datanya di ganti*/
+
+  print(array1);
+  print(array2);
+}
