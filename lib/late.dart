85b56efecbd5daf29bc5d4fefe0d7064f4a27fb8
IrfanFared <akunytberita527>
2024-03-08 22:24:23 +0700

    hari 3 belajar dart

---
+++ lib/late.dart
@@ -0,0 +1,18 @@
+/*late String nama;
+
+void main() {
+  nama = "irfan";
+
+  print(nama);
+}*/
+// contoh lain
+
+String namaLengkap() {
+  print("Irfan");
+  return "Pratama";
+}
+
+void main() {
+  late String velue = namaLengkap();
+  print(velue);
+}
