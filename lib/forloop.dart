d89a6d839db5e5b6b02737584c1a17b1ffb3c36e
IrfanFared <akunytberita527>
2024-06-02 11:25:34 +0700

    belajar perulangan di dart

---
+++ lib/forloop.dart
@@ -0,0 +1,27 @@
+void main() {
+  // for (;;) {
+  //   print("hello world");
+  // }
+  //kode diatas akan dijalankan tanpa henti jangan dilakukan karena membuat  laptop panas
+  //karena tidak ada init star=temn dan tidak ada kondisi
+
+  for (var angkatakhenti = 1; angkatakhenti <= 10; angkatakhenti++) {
+    print("irfan ganteng kelas $angkatakhenti");
+  }
+}
+
+///for di dart adalah kata kunci yang digunakan untuk perulagan 
+///kode for akan dieksekusu sampai kodisi tidak terpenuhi 
+///
+///sytax perulangan
+///
+///    for(init stetmen;kondisi;post stetment) {
+/// 
+///  block prulangan}
+/// ini stetmen hanya akan di eksekusi hanya sekali
+///  kodisi akan dilakukan pengecekan setiap mengasilkan tru maka kode akan dijalankan
+/// sampai menghasilkan kosdisi false
+/// init stetmen akan dieksekusi setiap akhir dari perulangan
+/// 
+/// 
+/// 
---
+++ lib/whileloop.dart
@@ -0,0 +1,8 @@
+void main() {
+  var counter = 1;
+  while (counter <= 10) {
+    print("perulangan ke $counter");
+    counter++;
+  }
+}
+/// while itu sebenernya sama seperti sama while loop cuma lebih sederhana ketimbang foor loop
