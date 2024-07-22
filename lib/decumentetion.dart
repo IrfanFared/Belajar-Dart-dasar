dabb187669f96fc30c525282e94009ddd56233b7
IrfanFared <akunytberita527>
2024-07-22 11:09:20 +0700

    push tipis tipis

---
+++ lib/decumentetion.dart
@@ -0,0 +1,5 @@
+/// statement adalah  unit syintax yang ada di sebuah bahasa pemogramn  yang menyatakan  aksi atau tugas untuk dilakukan 
+/// expresi adalah syntax pada bahsa pemograman yang menghasilkan sebuah nilai 
+/// syntax adalah aturan untuk menulim sebuah kode dalam sebuah bahasa pemograman supaya lebih terstruktu supaya komputer memahami apa yang kamu perintahkan
+/// intinya expresi menghasilkan sebuah nilai sedangkan statemen malakukan tugas tertentu
+/// 
